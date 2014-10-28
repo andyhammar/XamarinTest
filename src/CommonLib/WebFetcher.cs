0ebb807656e9516746fe76adc7496db91240a413
Andreas Hammar <andreas.hammar@jayway.com>
2014-10-28 08:56:44 +0100

    univeral app done

---
+++ src/CommonLib/WebFetcher.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace CommonLib
+{
+    public class WebFetcher
+    {
+
+        public async Task<string> GetWord()
+        {
+            var client = new HttpClient();
+            var json = await client.GetStringAsync("http://api.wordnik.com:80/v4/words.json/wordOfTheDay?api_key=06d5af30ca8a8fc0190080bb1da0a46e2718a483a4cf1cda9");
+            var root = JObject.Parse(json);
+            var word = root.GetValue("word").ToString();
+            return word;
+        }
+    }
+}
---
+++ src/UniversalWithXamarin/UniversalWithXamarin.Shared/StartPage.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+using CommonLib;
+using UniversalWithXamarin.Annotations;
+
+namespace UniversalWithXamarin
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class StartPage : Page, INotifyPropertyChanged
+    {
+        private string _word;
+        private bool _isBusy;
+
+        public StartPage()
+        {
+            this.InitializeComponent();
+            DataContext = this;
+            if (DesignMode.DesignModeEnabled)
+            {
+                Word = "my word";
+            }
+        }
+
+        private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var fetcher = new WebFetcher();
+            IsBusy = true;
+            try
+            {
+                var word = await fetcher.GetWord();
+                Word = word;
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+
+        }
+
+        public string Word
+        {
+            get { return _word; }
+            set
+            {
+                _word = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsBusy
+        {
+            get { return _isBusy; }
+            set { _isBusy = value; OnPropertyChanged(); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
