1bc1bd908c603d82325908c3ae5d15910c6c2171
Andreas Hammar <andreas.hammar@jayway.com>
2014-10-28 10:50:34 +0100

    added forms app, works on android

---
+++ src/Forms/Forms/App.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommonLib;
+using Xamarin.Forms;
+
+namespace Forms
+{
+    public class App
+    {
+        public static Page GetMainPage()
+        {
+            var textBlock = new Label()
+            {
+            };
+
+            var progress = new ProgressBar()
+            {
+                IsVisible = false,
+                VerticalOptions = LayoutOptions.Start,
+                HorizontalOptions = LayoutOptions.CenterAndExpand
+            };
+            return new ContentPage
+            {
+                //Content = new Label
+                //{
+                //    Text = "Hello, Forms !",
+                //    VerticalOptions = LayoutOptions.CenterAndExpand,
+                //    HorizontalOptions = LayoutOptions.CenterAndExpand,
+                //},
+
+                
+                Content = new StackLayout()
+                {
+                    Children =
+                    {
+                        new Label()
+                        {
+                          Text = "F O R M S",
+                          Font = Font.SystemFontOfSize(24),
+                          VerticalOptions = LayoutOptions.Start,
+                            HorizontalOptions = LayoutOptions.CenterAndExpand,
+
+                        },
+                        new Button()
+                        {
+                            Text = "get word",
+                            Command = new Command(async () =>
+                            {
+                                progress.IsVisible = true;
+                                var fetcher = new WebFetcher();
+                                var word = await fetcher.GetWord();
+                                textBlock.Text = word;
+                                progress.IsVisible = false;
+                            })
+                        },
+                        progress,
+                        textBlock
+                    }
+                }
+            };
+        }
+    }
+}
